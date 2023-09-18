533e58520b5991210beacacf44b960d4949c3db4
Eric <ehuang2467@gmail.com>
2023-09-17 22:14:32 -0500

    made static version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+
+const ads_json = [{
+  "ad_type": "sell",
+  "text": "selling bike", "user": "bob", "date": "1/20/2023"
+},
+{
+  "ad_type": "buy",
+  "text": "buy car", "user": "eric", "date": "4/23/2023"
+}]
+
+
+function Filterable_Ads() {
+  return (
+    <div className='filterable_ads'>
+      <AdType_Selector />
+      <SearchBar />
+      <AdsList ads={ads_json} />
+    </div>
+  )
+
+}
+
+function AdType_Selector() {
+  return (
+    <div className='adtype_selector'>
+      <select>
+        <option>Buy</option>
+        <option>Sell</option>
+      </select>
+    </div>
+  )
+
+}
+
+function SearchBar() {
+  return (
+    <div className='search_bar'>
+      <label>Search:</label>
+      <input type="text"></input>
+    </div>
+  )
+
+}
+
+function AdsList({ ads }) {
+  function ad_json_unpacker(ad) {
+    return ad.ad_type + ": " + ad.text + ". Posted by " + ad.user + " on " + ad.date
+  }
+  function ad_json_key(ad) {
+    return ad.user + ad.text
+  }
+  let lis = ads.map((item) => <li key={ad_json_key(item)}>{ad_json_unpacker(item)}</li>)
+  return (
+    <div className='ads_list'>
+      <ul>{lis}</ul>
+    </div>
+  )
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Filterable_Ads />
   );
 }
 
